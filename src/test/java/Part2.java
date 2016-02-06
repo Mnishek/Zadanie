e8027b471e8a5b80a8f4b1e31ffa36c4428d9fa2
Mnishek <Bgbghghg1>
2016-02-06 22:32:22 +0300

    Your username is invalid!

--- src/test/java/Part2.java
+++ src/test/java/Part2.java
@@ -3,12 +3,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-public class Zadanie2 {
-
+/**
+ * Created by Marina on 06.02.16.
+ */
+public class Part2 {
     @Test
     public void login(){
         WebDriver test = new FirefoxDriver();
@@ -18,17 +23,18 @@ public class Zadanie2 {
         cssClick.click();
         test.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         WebElement cssLog = test.findElement(By.id("username"));
-        cssLog.sendKeys("LOlipop");
+        cssLog.sendKeys("tomsmith");
         WebElement cssPass = test.findElement(By.id("password" ));
-        cssPass.sendKeys("123456");
+        cssPass.sendKeys("SuperSecretPassword!");
         WebElement cssClick2 = test.findElement(By.cssSelector(".radius"));
         cssClick2.click();
-        WebElement cssErorr = test.findElement(By.id("flash"));
-        System.out.println(cssErorr.getText());
-        Assert.assertTrue(cssErorr.getText().contains("Your username is invalid!"));
+        WebElement cssOk = test.findElement(By.id("flash"));
+        Assert.assertTrue(cssOk.getText().contains("You logged into a secure area!"));
         test.quit();
 
 
     }
+
+
 
 }
