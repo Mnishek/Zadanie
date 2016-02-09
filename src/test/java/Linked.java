abf0c15523eb11c40c4d6d355277a629d48f280e
Mnishek <Bgbghghg1>
2016-02-09 12:06:05 +0300

    LinkedList

---
+++ src/test/java/Linked.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+
+/**
+ * Created by Marina on 09.02.16.
+ */
+public class Linked {
+
+        public static void main(String args[]) {
+
+            LinkedList<String> number = new LinkedList<String>();
+            number.add("One");
+            number.add("Two");
+            number.add("Three");
+            number.add("Four");
+            number.add("Five");
+
+            System.out.println(number);
+        }
+}
